52bde8b3ec6ba4cdc4bcb539df502534358e681f
javaDev-cloud <munish6330@gmail.com>
2025-09-11 11:05:42 -0400

    events mouse events click events input events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoginComponent, SignupComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'angular-19-tut';
+  count: number = 0;
+
+  handleClickbutton(val: String) {
+    console.log(val);
+    if (val == 'plus') {
+      this.count++;
+    } else if (val == 'minus' && this.count > 0) {
+      this.count--;
+    } else {
+      this.count = 0;
+    }
+  }
+
+  handleEvent(event: Event) {
+    console.log('event triggered: ', event.type);
+    console.log('event triggered: ', (event.target as HTMLInputElement).name);
+    console.log('event triggered: ', (event.target as HTMLInputElement).value);
+  }
 }
